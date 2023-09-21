4ff70cb0b0149820d2a01ed9ccc3e795b4b5ec43
Romangonzalezz <romanynaarut@gmail.com>
2023-09-20 22:54:37 -0300

    Punto 2 listo

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-test
+require('dotenv').config.js
+
+
+const Server = require('./models/server');
+
+const svr = new Server();
+
+
+svr.listen();
+
+
+//2. Visualización de un registro en particular (param id)
+
+//3. Listado de registros en formato json que pueda filtrarse a través de query params. (categoría, idioma,
+//álbum, género, etc.)
+
+//4. En caso de entrega grupal, cada alumno deberá cubrir los puntos del 1 al 3 en su entrega.
+
+//Aclaración: Devolver los status code en cada request (200, 401, 404 ,etc.)
--- routes/demo.js
+++ routes/demo.js
@@ -4,7 +4,7 @@ const { getRegistrosJSON, getCaracterID } = require('../controllers/demo');
 const rutas = Router();
 
 rutas.get('/registrosJSON', getRegistrosJSON);
-rutas.get('/caracterID/:id', getRegistrosJSON);
+rutas.get('/caracterID/:id', getCaracterID);
 
 
 module.exports = rutas;
